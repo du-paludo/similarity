a7dcd928591b960663e8dd58739000b65dc0788c
Eduardo Paludo <esp21@inf.ufpr.br>
2023-01-29 19:44:43 -0300

    custo adicionado

--- comp-alg1-alg4-3.py
+++ comp-alg1-alg4-3.py
@@ -1,22 +1,24 @@
 import networkx as nx
-import itertools
 import random
 import time
 import math
 
+# O(len_u)
 def similarity(sim1, u, v):
-    neighbors_u = list(G.neighbors(u))
+    neighbors_u = list(G.neighbors(u))  # O(len_u)
     # Como u e v não precisam ser vizinhos, remove da lista caso sejam
     if v in neighbors_u:
-        neighbors_u.remove(v)
-    len_u = len(neighbors_u)
+        neighbors_u.remove(v)   # O(1)
+    len_u = len(neighbors_u)    # O(1)
 
-    neighbors_v = list(G.neighbors(v))
+    neighbors_v = list(G.neighbors(v))  # O(len_v)
     if u in neighbors_v:
         neighbors_v.remove(u)
     len_v = len(neighbors_v)
 
-    common_neighbors = list(nx.common_neighbors(G, u, v))
+    common_neighbors = list(nx.common_neighbors(G, u, v))   # O(len_U)
+    #return (w for w in G[u] if w in G[v] and w not in (u, v))
+
     len_common = len(common_neighbors)
 
     if (len_u + len_v - len_common) == 0:
@@ -42,9 +44,9 @@ sim1 = {}
 
 start_time = time.time()
 
-for i in range(len(nodes)):
-    for j in range(i + 1, len(nodes)):
-        similarity(sim1, nodes[i], nodes[j])
+for i in range(len(nodes)):     # O(n)
+    for j in range(i + 1, len(nodes)):      # O(n)
+        similarity(sim1, nodes[i], nodes[j])        # O(len_i)
 
 print("Time of execution of algorithm 1: %s seconds" % (time.time() - start_time))
 
@@ -59,20 +61,18 @@ signal_array = {}
 
 start_time = time.time()
 
-for node in nodes:
-    for i in G.neighbors(node):
-    #for i in random.choices(nodes, k=int(math.log(n, 1.1)/2)):
-        #for j in random.choices(nodes, k=int(math.log(n, 1.1))):
-        for j in random.choices(nodes, k=30):
-        #for j in random.sample(nodes, k=n):
+for node in nodes:      # O(n)
+    for i in G.neighbors(node):     # O(len_neighbors)
+        for j in random.choices(nodes, k=math.sqrt(n)):      # O(sqrt(n))       
+        #for j in random.sample(nodes, k=math.sqrt(n)):      # O(sqrt(n))  
             if j == i or j == node or (frozenset((i, j)) in signal_array):
                 continue
-            if i in G.neighbors(node) and j in G.neighbors(node):
-                sim2[frozenset((i, j))][0] += 1
+            if i in G.neighbors(node) and j in G.neighbors(node):       # O(len_neighbors)
+                sim2[frozenset((i, j))][0] += 1                         # O(1)
             if i in G.neighbors(node) or j in G.neighbors(node):
                 sim2[frozenset((i, j))][1] += 1
             signal_array[frozenset((i, j))] = 0
-    signal_array.clear()
+    signal_array.clear()    # O(len(signal_array))
 
 print("Time of execution of algorithm 2: %s seconds" % (time.time() - start_time))
 
